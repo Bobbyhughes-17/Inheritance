67c0a6c04368b8370c887b88848171f121c34bfd
Bobby Hughes <bobbyhughes6316@gmail.com>
2023-06-05 09:59:55 -0400

    inheritance

---
+++ Ram.cs
@@ -0,0 +1,14 @@
+namespace Garage
+{
+    public class Ram : Vehicle
+    {
+        public double FuelCapacity { get; set; }
+        public string MainColor { get; set; }
+        public int MaximumOccupancy { get; set; }
+
+        public void RefuelTank()
+        {
+            // method definition omitted
+        }
+    }
+}
